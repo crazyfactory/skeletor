8b3b78a77ef7b98fa1a22bb56c1f94be1d87684e
Nishchal <n.gautam@crazy-factory.com>
2016-09-08 10:32:56 +0700

    added polyfill

--- src/Create.php
+++ src/Create.php
@@ -5,7 +5,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-
+require_once __DIR__.'/polyfill.php';
 class Create extends Command
 {
 	private $use_defaults;
---
+++ src/polyfill.php
@@ -0,0 +1,9 @@
+<?php
+if(!function_exists('readline')){
+	function readline($string){
+		echo $string;
+		$line = fgets(STDIN);
+		$line=rtrim($line, "\n");
+		return $line;
+	}
+}
